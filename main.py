5048c875048f0a9b1e9c1a34821427c0c174f321
Yulia <33177996+udsey@users.noreply.github.com>
2024-02-09 20:24:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from kivy.app import App
 from kivy.uix.videoplayer import VideoPlayer
 
 
-class APPPP(App):
+class APP(App):
     def build(self):
         video = VideoPlayer(source='Historia_de_la_ciencia.mp4')
         return video
 
 
 if __name__ == '__main__':
-    APPPP().run()
+    APP().run()
